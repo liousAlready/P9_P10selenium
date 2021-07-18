ce212ac0f24b5ee478dd6d90d926f7631adfb90b
lishouwu <m15574933885@163.com>
2021-07-18 15:22:27 +0800

    demo 提交

--- class3/demo_wait2.py
+++ class3/demo_wait2.py
@@ -1,24 +1,15 @@
 # -*- coding: utf-8 -*-
-# @Time    : 2021/7/18 2:55 下午
+# @Time    : 2021/7/18 3:10 下午
 # @Author  : Li
 # @Email   : m15574933885@163.com
-# @File    : demo_wait.py
+# @File    : demo_wait2.py
 # @Software: PyCharm
-
-
-# 等待操作
 
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 STYLE = "background: blue; border: 2px solid red;"  # 高亮的样式
-
-
-def find(driver, by, expr):
-    element = driver.find_element(by, expr)
-    driver.execute_script("arguments[0].setAttribute('style', arguments[1]);", element, STYLE)
-    return element
 
 
 def highLightElement(driver, element):
@@ -31,15 +22,32 @@ def highLightElement(driver, element):
 
 # 1.扩展知识：元素高亮
 driver = webdriver.Chrome()
-driver.get("https://www.baidu.com")
-# find(driver, By.NAME, 'wd').click()
-el = driver.find_element(By.NAME, 'wd')
-highLightElement(driver, el)
+driver.get("file:///Users/lishouwu/Downloads/Wait.html")
 
 # 2.不加任何等待，直接报错
 
+# driver.find_element(By.ID,'b').click()
+# el = driver.find_element(By.CSS_SELECTOR, 'div.red_box')
+# highLightElement(driver, el)
+
 # 3.sleep()：固定休眠时间设置 固定等待7s
+
+# driver.find_element(By.ID,'b').click()
+# time.sleep(7)
+# el = driver.find_element(By.CSS_SELECTOR, 'div.red_box')
+# highLightElement(driver, el)
 
 # 4.implicitlyWait()：隐式等待,  加隐式等待10秒
 
+# driver.implicitly_wait(10)  # 只需要设置一次，整个代码都生效
+# driver.find_element(By.ID, 'b').click()
+# el = driver.find_element(By.CSS_SELECTOR, 'div.red_box')
+# highLightElement(driver, el)
+
 # 5.WebDriverWait():显示等待，加显示10s
+
+
+
+time.sleep(2)
+
+driver.quit()
