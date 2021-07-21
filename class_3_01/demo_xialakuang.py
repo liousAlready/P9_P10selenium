c6ef0ec4f25d93b11e06f4d90d2fb96c22343829
lishouwu <m15574933885@163.com>
2021-07-21 20:13:10 +0800

    作业上传

---
+++ class_3_01/demo_xialakuang.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/7/21 8:11 下午
+# @Author  : Li
+# @Email   : m15574933885@163.com
+# @File    : demo_xialakuang.py
+# @Software: PyCharm
+
+import time
+from selenium import webdriver
+from selenium.webdriver.support.select import Select
+
+# 下拉框的处理
+
+# 方法一： 直接定位元素
+
+
+# 方法二: 层级定位
+
+
+# 方法三：使用select对象
